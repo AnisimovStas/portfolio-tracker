49326911ad48abf6daac8ec0d525906fa6ba957d
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2024-02-24 14:18:10 +0400

    feat: added components for charts

---
+++ frontend/layers/Portfolio/components/Chart/Chart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="chart__container">Cам график {{ data }}</div>
+</template>
+<script setup lang="ts">
+export interface IChartProps {
+  data: unknown[];
+}
+
+defineProps<IChartProps>();
+</script>
+<style scoped>
+.chart__container {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+</style>
--- frontend/layers/Portfolio/components/GeneralGhart/GeneralGhart.vue
+++ frontend/layers/Portfolio/components/GeneralGhart/GeneralGhart.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="charts__container">
-    <!--    <UCarousel v-slot="{ item }" :items="items">-->
-    <!--      <p>{{ item }}</p>-->
-    <!--    </UCarousel>-->
-    123
+    <div v-for="item in items" :key="item.value">
+      <Chart v-if="activeChart === item.value" :data="item.data" />
+    </div>
+
+    <UTabs v-model="activeChart" :items="items" />
   </div>
 </template>
 <script setup lang="ts">
-const items = ["График общей стоимости потрфеля", "График профита"];
+import Chart from "~/layers/Portfolio/components/Chart/Chart.vue";
+
+const activeChart = ref(1);
+
+const items = [
+  {
+    data: ["График общей стоимости"],
+    label: "График общей стоимости",
+    value: 0,
+  },
+  { data: ["График профита"], label: "График профита", value: 1 },
+];
 </script>
 
 <style scoped>
 .charts__container {
   display: flex;
+  flex-direction: column;
+  gap: 10px;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+  width: 100%;
   height: 300px;
   border: 1px solid;
 }
