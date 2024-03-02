240dd5ef11a8873e62df5260319208facf9e5cb7
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2024-03-02 20:34:53 +0400

    fix: fixed google Auth communicate

--- backend/src/auth/utils/GoogleStrategy.ts
+++ backend/src/auth/utils/GoogleStrategy.ts
@@ -3,15 +3,21 @@ import { Strategy } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 import { AuthService } from '../auth.service';
 import { VerifyCallback } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {
     super({
       clientID:
         '525898649995-epsvkq17tdatpjdd504bg8iii8a1rml2.apps.googleusercontent.com',
       clientSecret: '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',
-      callbackURL: 'http://localhost:9229/api/auth/google/redirect',
+      callbackURL: `${configService.get(
+        'FRONTEND_BASE_URL',
+      )}/api/auth/google/redirect`,
       scope: ['profile', 'email'],
     });
   }
